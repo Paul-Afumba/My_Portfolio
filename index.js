78ab4771a9503d6f0a005826828fac2f1c41248e
Paul Afumba <paulafumba58@gmail.com>
2026-03-03 19:40:12 +0200

    Reussite du menu humburger, du bouton de contact.

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const menulinks = document.getElementById("menu-links");
 
 menuhamburger.addEventListener("click", () => {
     menulinks.classList.toggle("active");
+    menuhamburger.classList.toggle("active");
 });
