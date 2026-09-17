08215d7f1e1c7b71a80f76b528315e2f3457026a
Paul Afumba <paulafumba58@gmail.com>
2026-09-17 19:35:07 +0200

    navbar improvement (scroll and appearence ) , deletion of form and CTA section

--- index.js
+++ index.js
@@ -5,3 +5,30 @@ menuhamburger.addEventListener("click", () => {
     menulinks.classList.toggle("active");
     menuhamburger.classList.toggle("active");
 });
+
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+// Définition de la marge de tolérance en pixels
+const tolerance = 15; 
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Calcule l'écart entre la position actuelle et la précédente
+  const scrollDelta = Math.abs(scrollTop - lastScrollTop);
+
+  // Si le défilement est plus petit que la tolérance, on ne fait rien
+  if (scrollDelta < tolerance) {
+    return; 
+  }
+
+  if (scrollTop > lastScrollTop && scrollTop > 90) {
+    // Scroll vers le bas ET on a dépassé la hauteur de la navbar -> Masquer
+    navbar.classList.add('navbar-hidden');
+  } else if (scrollTop < lastScrollTop) {
+    // Scroll vers le haut -> Afficher
+    navbar.classList.remove('navbar-hidden');
+  }
+  
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+});
